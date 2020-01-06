a204680f6a3783351a41dd82508591ced361af6a
xmbmaster <52519864+xmbmaster@users.noreply.github.com>
2020-01-06 14:26:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.13
+    cd 3proxy-0.8.13
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
